59f9c2b18d0dd955ca69eef9ca54236191acd4d9
maRia-1506A <zannatulfarzana363@gmail.com>
2025-12-20 01:23:48 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        echo "hello "."maria"."<br>";
+        
+        // variable
+        $name= 'maria';
+        $age= 21;
+        $isFather= true;
+        $money= 500.50;
+        $salary=null;
+
+        echo "My name is ".$name."<br>";
+        echo $age."<br>";
+        echo $isFather."<br>";
+        echo $money."<br>";
+        echo $salary."<br>";
+    ?>
+</body>
+</html>
